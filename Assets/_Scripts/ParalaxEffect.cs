4dcc9c3585d31c639baab27a99f842b7ca0b5b04
Gab <gamajorgensen@outlook.com>
2025-09-29 13:35:25 +0200

    Parallax effect

---
+++ Assets/_Scripts/ParalaxEffect.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class ParalaxEffect : MonoBehaviour
+{
+    private float length, startpos;
+    public GameObject cam;
+    public float parallaxEffect;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        startpos = transform.position.x;
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float temp = (cam.transform.position.x * (1 - parallaxEffect));
+        float dist = (cam.transform.position.x * parallaxEffect);
+
+        transform.position = new Vector3(startpos + dist, transform.position.y, transform.position.z);
+
+        if (temp > startpos + length) startpos += length;
+        else if (temp < startpos - length) startpos -= length;
+    }
+}
